4cb85e1f4eb24abbf17dc81fa3ea68fb090504ba
Victor Pukman <pukman.victor@gmail.com>
2020-08-30 01:00:43 +0300

    blend anim added

--- hero.gd
+++ hero.gd
@@ -5,9 +5,11 @@ var gravity = -9.8
 var velocity = Vector3()
 var camera
 
-const SPEED = 6
+const SPEED = 3
 const ACCEL = 3
-const DE_ACCEL = 5
+const DE_ACCEL = 10
+
+#onready var anim = $HeroMesh2/AnimationPlayer
 
 
 func _ready():
@@ -15,15 +17,20 @@ func _ready():
 
 func _physics_process(delta):
 	var dir = Vector3()
+	var is_moving = false
 	
 	if Input.is_action_pressed("ui_up"):
 		dir += -camera.basis[2]
+		is_moving = true
 	elif Input.is_action_pressed("ui_down"):
 		dir += camera.basis[2]
+		is_moving = true
 	elif Input.is_action_pressed("ui_left"):
 		dir += -camera.basis[0]
+		is_moving = true
 	elif Input.is_action_pressed("ui_right"):
 		dir += camera.basis[0]
+		is_moving = true
 		
 	dir.y = 0
 	dir = dir.normalized()
@@ -46,3 +53,13 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, Vector3(0,1,0))
 	
+	
+#	var anim_id = "Idle"
+#	if is_moving:
+#		anim_id = "Run"
+#
+#	if anim.get_current_animation() != anim_id:
+#		anim.play(anim_id)
+	
+	var speed = hv.length() / SPEED
+	$HeroMesh/AnimationTreePlayer.blend2_node_set_amount("Idle_Run", speed) #.blend2_node_set_amount("Idle_Run", speed)
